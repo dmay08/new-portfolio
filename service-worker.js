913b1023d64b9b5b4a76a2114422a694d09a9cfb
Danny Mayasich <dmayasich@macmini-ops05.avtr.net>
2019-08-26 16:32:56 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/new-portfolio/precache-manifest.57c53d7dd5709a8a5acd70d335f44434.js"
+  "dannymayasich.com/precache-manifest.e1dd082029fd29e7adafd035b8f08b62.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/new-portfolio/index.html", {
+workbox.routing.registerNavigationRoute("dannymayasich.com/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
